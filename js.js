d5b9ecb6fd1aa57b0c060162f44e764d3ab6a937
Suchet <suchetchava@gmail.com>
2025-05-21 16:37:37 +0000

    Feeding my family

--- js.js
+++ js.js
@@ -1,5 +1,14 @@
-var LIST = {"forzen burritos", "banna foster", "Egg mcMuffin with egg"}
 
-for(let i = 0; i < 3; i++){
-alert("Frozen Burritos")
-};
+
+var BTN = document.getElementById("mybutton");
+var doSomething = function(event){
+    alert("praise me! ");
+    console.log(event);
+}
+
+for (var i = 0; i < 3; i++){
+    var newBTN = BTN.cloneNode(true);
+    document.body.append(newBTN)
+    newBTN.addEventListener("click", doSomething)
+}
+alert("Please let it work")
